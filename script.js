2c655c2ea34afd05426618aea27faaa3718c02df
srilakshmi <srilakshmipeddineni835@gmail.com>
2026-05-05 17:06:06 +0530

    Commiting last changes

--- script.js
+++ script.js
@@ -14,18 +14,8 @@ actualForm.addEventListener('submit', function (event) {
   event.preventDefault();
   formContainer.style.display = 'none';
   successMessage.style.display = 'block';
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const reveals = document.querySelectorAll('.reveal-left, .reveal-right');
-
-  window.addEventListener('scroll', () => {
-    reveals.forEach(el => {
-      const top = el.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      if (top < windowHeight - 100) {
-        el.classList.add('active');
-      }
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
   });
 });
